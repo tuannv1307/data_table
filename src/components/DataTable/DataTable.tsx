74ab0b37a5ea49e7dfb254934ca09ddc7d91b28a
Nguyễn Tuấn <nvt13071@gmail.com>
2022-12-12 21:10:55 +0700

    update

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -9,31 +9,31 @@ const DataTable = () => {
     <table id="example" className={st(classes.root)}>
       <thead>
         <tr>
-          <th className={st(classes.name)}>Name</th>
-          <th className={st(classes.position)}>Position</th>
-          <th className={st(classes.office)}>Office</th>
-          <th className={st(classes.extn)}>Extn.</th>
-          <th className={st(classes.StartDate)}>Start date</th>
+          <th className={st(classes.name, classes.sorting)}>Name</th>
+          <th className={st(classes.position, classes.sorting)}>Position</th>
+          <th className={st(classes.office, classes.sorting)}>Office</th>
+          <th className={st(classes.extn, classes.sorting)}>Extn.</th>
+          <th className={st(classes.StartDate, classes.sorting)}>Start date</th>
           {/* <th>Salary</th> */}
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-        </tr>
-        <tr>
-          <td>1</td>
+          <td className={st(classes.dtrControl)}>1</td>
           <td>1</td>
           <td>1</td>
           <td>1</td>
           <td>1</td>
         </tr>
         <tr>
-          <td>2</td>
+          <td className={st(classes.dtrControl)}>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td className={st(classes.dtrControl)}>2</td>
           <td>2</td>
           <td>2</td>
           <td>2</td>
